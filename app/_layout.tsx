b751b3ed3e26407874aa899733c86fc4926bf9bc
markmckay <info@spokentext.net>
2025-08-12 15:04:44 -0400

    Remove duplicate useFrameworkReady import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 export default function RootLayout() {
   useFrameworkReady();
